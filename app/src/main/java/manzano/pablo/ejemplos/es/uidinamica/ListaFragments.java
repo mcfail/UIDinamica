f4ca70671d8856bb2d8f6548e2a5a431c080c4cd
mcfail <pblmanzan@gmai.com>
2015-02-19 17:03:59 +0100

    Commit inicial

---
+++ app/src/main/java/manzano/pablo/ejemplos/es/uidinamica/ListaFragments.java
@@ -0,0 +1,9 @@
+package manzano.pablo.ejemplos.es.uidinamica;
+
+import android.support.v4.app.ListFragment;
+
+public class ListaFragments extends ListFragment {
+
+    private ItemlistSelected listener;
+
+}
